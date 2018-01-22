13120315dcc05ddfb4734d03dc583a0c575f589c
Jose Raul <joseraul.perezfrias@gmail.com>
2018-01-22 19:48:33 +0100

    add some comments and tests

--- lesson_2/CyclicRotation.php
+++ lesson_2/CyclicRotation.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * A zero-indexed array A consisting of N integers is given.
+ * Rotation of the array means that each element is shifted right by one index,
+ * and the last element of the array is moved to the first place.
+ * For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]
+ * (elements are shifted right by one index and 6 is moved to the first place).
+ *
+ * @param $array_to_rotate
+ * @param $times
+ * @return array
+ */
 function solution($array_to_rotate, $times)
 {
     if(0 === count($array_to_rotate)){
@@ -11,3 +21,13 @@ function solution($array_to_rotate, $times)
     }
     return $array_to_rotate;
 }
+
+// Correctness tests
+// extreme_empty
+echo solution([], 0);
+echo solution([], 1);
+
+// single
+echo solution([-4], 0);
+echo solution([0], 0);
+echo solution([1000], 0);
---
+++ lesson_3/PermMissingElem.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jose Raul
+ * Date: 22/01/2018
+ * Time: 19:48
+ */
