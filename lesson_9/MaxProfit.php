20da69ffe52c3c015412b0ea75e9cae354f08c92
Jose Raul <joseraul.perezfrias@gmail.com>
2018-02-09 18:41:02 +0100

    correct 100%, performance 25%

--- lesson_9/MaxProfit.php
+++ lesson_9/MaxProfit.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jose Raul
- * Date: 09/02/2018
- * Time: 18:28
- */
+
+function solution($A)
+{
+    $sum = 0;
+    for($i=0; $i < count($A); $i++) {
+        for($z=($i+1); $z < count($A); $z++) {
+            if (($A[$z] - $A[$i]) > $sum) {
+                $sum = ($A[$z] - $A[$i]);
+            }
+        }
+    }
+    return $sum;
+}
+
+echo solution([23171, 21011, 21123, 21366, 21013, 21367]); // 356
